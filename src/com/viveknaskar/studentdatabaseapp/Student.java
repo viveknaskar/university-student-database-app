d70ca5e0904be3cb22a82f7aef8574206cd16b22
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-06 15:46:24 +0530

    Added enrollment logic for courses

--- src/com/viveknaskar/studentdatabaseapp/Student.java
+++ src/com/viveknaskar/studentdatabaseapp/Student.java
@@ -8,10 +8,10 @@ public class Student {
     private String lastName;
     private int gradeYear;
     private String studentId;
-    private String courses;
-    private int tuitionBalance;
+    private String courses = null;
+    private int tuitionBalance = 0;
     private static int costOfCourse = 600;
-    private static int id = 1001;
+    private static int id = 1000;
 
     //Constructor: prompt user to enter Student's name and year
     public Student() {
@@ -36,8 +36,24 @@ public class Student {
         this.studentId =  gradeYear + " " + id;
     }
 
-
     //Enroll in courses
+    public void enroll() {
+        // Get inside the loop, user hits 0
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+
+            if (!course.equals("Q")) {
+                courses += "\n" + course;
+                tuitionBalance += costOfCourse;
+            } else {
+                break;
+            }
+        } while (1 != 0);
+        System.out.println("Enrolled in " + courses);
+        System.out.println("Tuition Balance is " + tuitionBalance);
+    }
 
     //View Balance
 
--- src/com/viveknaskar/studentdatabaseapp/StudentDatabaseApp.java
+++ src/com/viveknaskar/studentdatabaseapp/StudentDatabaseApp.java
@@ -5,6 +5,7 @@ public class StudentDatabaseApp {
     public static void main(String[] args) {
 
         Student student1 = new Student();
+        student1.enroll();
 
     }
 }
