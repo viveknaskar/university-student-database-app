4bfc83cec6e85796d9afcc4e3e40f4a5a0fcfcb7
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-06 14:21:55 +0530

    Created the basic structure

---
+++ src/com/viveknaskar/studentdatabaseapp/Student.java
@@ -0,0 +1,26 @@
+package com.viveknaskar.studentdatabaseapp;
+
+public class Student {
+
+    private String firstName;
+    private String lastName;
+    private String gradeYear;
+    private int studentId;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    //Constructor: prompt user to enter Student's name and year
+    public Student() {
+
+    }
+
+    //Generate an ID
+
+    //Enroll in courses
+
+    //View Balance
+
+    //Pay tuition
+
+}
---
+++ src/com/viveknaskar/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,8 @@
+package com.viveknaskar.studentdatabaseapp;
+
+public class StudentDatabaseApp {
+
+    public static void main(String[] args) {
+
+    }
+}
