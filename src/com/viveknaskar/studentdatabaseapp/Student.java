0f9ef227be5aa492a39df4e0488a07177a1ebbfd
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-06 14:36:22 +0530

    Initialized the Student constructor

--- src/com/viveknaskar/studentdatabaseapp/Student.java
+++ src/com/viveknaskar/studentdatabaseapp/Student.java
@@ -1,10 +1,12 @@
 package com.viveknaskar.studentdatabaseapp;
 
+import java.util.Scanner;
+
 public class Student {
 
     private String firstName;
     private String lastName;
-    private String gradeYear;
+    private int gradeYear;
     private int studentId;
     private String courses;
     private int tuitionBalance;
@@ -12,7 +14,16 @@ public class Student {
 
     //Constructor: prompt user to enter Student's name and year
     public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter student first name: ");
+        this.firstName = in.nextLine();
+        System.out.println("Enter student last name: ");
+        this.lastName = in.nextLine();
+        System.out.println("1 - First year\n2 - Second year\n3 - Third year\n4 - Final year\n" +
+                "Enter student grade year: ");
+        this.gradeYear = in.nextInt();
 
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
     }
 
     //Generate an ID
--- src/com/viveknaskar/studentdatabaseapp/StudentDatabaseApp.java
+++ src/com/viveknaskar/studentdatabaseapp/StudentDatabaseApp.java
@@ -4,5 +4,7 @@ public class StudentDatabaseApp {
 
     public static void main(String[] args) {
 
+        Student student1 = new Student();
+
     }
 }
