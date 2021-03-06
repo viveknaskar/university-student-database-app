4dd8d203851bbef07041f86f2365321353ea47d6
Vivek Naskar <viveknaskar2912@gmail.com>
2021-03-06 15:01:47 +0530

    Created unique student id

--- src/com/viveknaskar/studentdatabaseapp/Student.java
+++ src/com/viveknaskar/studentdatabaseapp/Student.java
@@ -7,10 +7,11 @@ public class Student {
     private String firstName;
     private String lastName;
     private int gradeYear;
-    private int studentId;
+    private String studentId;
     private String courses;
     private int tuitionBalance;
-    private int costOfCourse = 600;
+    private static int costOfCourse = 600;
+    private static int id = 1001;
 
     //Constructor: prompt user to enter Student's name and year
     public Student() {
@@ -23,10 +24,18 @@ public class Student {
                 "Enter student grade year: ");
         this.gradeYear = in.nextInt();
 
-        System.out.println(firstName + " " + lastName + " " + gradeYear);
+        setStudentId();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentId);
+
     }
 
-    //Generate an ID
+    //Generate a unique student ID
+    private void setStudentId() {
+        // StudentId = Grade level + Id
+        id++;
+        this.studentId =  gradeYear + " " + id;
+    }
+
 
     //Enroll in courses
 
